ce350eaa0c8129fe92ac46258144167ce9374ab2
100rabh2812 <saurabhkedarkar28@gmail.com>
2025-06-14 16:17:15 +0100

    portfolio skill and project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,61 +3,90 @@ export const projectsInfo = {
   subtitle: "Recent Works & Achievements",
   projects: [
     {
-      title: "E-Commerce Platform",
+      title: "Acoustic Office",
       description:
-        "A full-stack e-commerce solution with React, Node.js, and MongoDB",
+        "Creating efficient, happier workplaces with soundproof pods, booths, furniture and more.",
       image:
-        "https://img.freepik.com/free-photo/document-marketing-strategy-business-concept_53876-132231.jpg",
-      tags: ["React", "Node.js", "MongoDB", "Redux"],
-      liveLink: "#",
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913668/acousticoffice_bljfa9.png",
+      tags: ["php", "wordpress", "jQuery", "mySQL"],
+      liveLink: "https://acousticoffice.uk/",
       githubLink: "#",
     },
     {
-      title: "Task Management App",
+      title: "Majestic Gen",
       description:
-        "Real-time task management application with drag-and-drop functionality",
+        "Majestic Gen is on a sustainable innovation journey, putting you first with ambition, honesty, and resourcefulness. We're building the future of sustainable developments with flair.",
       image:
-        "https://images.unsplash.com/photo-1572177812156-58036aae439c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2plY3RzfGVufDB8fDB8fHww",
-      tags: ["React", "Firebase", "Material-UI", "Redux"],
-      liveLink: "",
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913668/majesticgen_zf6hlg.png",
+      tags: ["php", "wordpress", "jQuery", "mySQL"],
+      liveLink: "https://majesticgen.com.my/",
       githubLink: "",
     },
     {
-      title: "Social Media Dashboard",
+      title: "Swiss Cars",
+      description: "Make Your Ride Easy & Fast with Swiss Cars",
+      image:
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913670/swiss_car_tbj1fw.png",
+      tags: ["Wordpress", "Elementor", "javascript", "mySQL"],
+      liveLink: "https://swisscars.com/",
+      githubLink: "",
+    },
+    {
+      title: "RR kabel",
       description:
-        "Analytics dashboard for social media metrics and engagement",
+        "RR Kabel | Leading Wire & Cable Manufacturer Company in India",
       image:
-        "https://www.simplilearn.com/ice9/free_resources_article_thumb/project_management_coursefees.jpg",
-      tags: ["Vue.js", "D3.js", "Node.js", "PostgreSQL"],
-      liveLink: "",
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913668/rr_kabel_t6pxme.png",
+      tags: ["Wordpress", "wordpress Block", "JavaScript", "mySQL"],
+      liveLink: "https://www.rrkabel.com/",
       githubLink: "",
     },
     {
-      title: "AI Image Generator",
-      description: "Web application that generates images using AI algorithms",
+      title: "Syncezy",
+      description: "SYNC your business apps the EZY way!",
       image:
-        "https://www.berlinsbi.com/uploads/sites/2/2023/08/msc-pm.webp?w=1920&h=1080&crop=1",
-      tags: ["Python", "TensorFlow", "React", "Flask"],
-      liveLink: "",
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913669/syncezy_p7d7k0.png",
+      tags: ["php", "wordpress", "jQuery", "mySQL"],
+      liveLink: "https://syncezy.com/",
       githubLink: "",
     },
     {
-      title: "Crypto Tracker",
-      description: "Real-time cryptocurrency tracking and portfolio management",
+      title: "Mansad",
+      description: "Crafting Spaces with Precision, Passion, and Care",
       image:
-        "https://emeritus.org/in/wp-content/uploads/sites/3/2022/03/What-is-project-management-Meaning-and-Definition-of-Project-Management-1.jpg.optimal.jpg",
-      tags: ["React", "WebSocket", "Chart.js", "API"],
-      liveLink: "",
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913668/mansad_hbwdje.png",
+      tags: ["php", "wordpress", "jQuery", "mySQL"],
+      liveLink: "https://www.mansad.co.uk/",
       githubLink: "",
     },
     {
-      title: "Weather App",
+      title: "PrecisionX",
       description:
-        "Weather forecast application with beautiful UI and animations",
+        "Crafting Spaces with Precision, Passion, and CareLooking to sell your car? List it with us today on a platform designed exclusively for performance cars and enthusiasts.",
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkU4HvXH-ZGJ1NFuBkeBrv3Tp0cPfGrnPWyQ&s",
-      tags: ["React", "OpenWeather API", "SCSS", "Axios"],
-      liveLink: "",
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913668/precisionx_fllpev.png",
+      tags: ["php", "wordpress", "woocommerce", "API", "javascript", "mySQL"],
+      liveLink: "https://precisionx.co.uk/",
+      githubLink: "",
+    },
+    {
+      title: "stxfinance",
+      description:
+        "manufacturing, selling, and financing luxury vehicles, the Stephex Group proudly presents STX Finance, your premier destination for luxury vehicle financing.",
+      image:
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913669/stxfinance_vtcjk1.png",
+      tags: ["php", "wordpress", "multilingual", "API", "javascript", "mySQL"],
+      liveLink: "https://www.stxfinance.com/",
+      githubLink: "",
+    },
+    {
+      title: "Mill Bank",
+      description:
+        "Expertly delivered services to keep your home flowing smoothly, every time. From minor fixes to major installations, we handle it all with precision and care.",
+      image:
+        "https://res.cloudinary.com/dqlvyzz4i/image/upload/v1749913667/millbank_e20043.png",
+      tags: ["php", "wordpress", "API", "javascript", "mySQL"],
+      liveLink: "https://millbankpropertymaintenance.co.uk/",
       githubLink: "",
     },
   ],
--- src/data/skills.js
+++ src/data/skills.js
@@ -5,30 +5,30 @@ export const skillsInfo = {
     {
       name: "Frontend Development",
       roles: [
-        { name: 'React.js', level: 75 },
-        { name: 'JavaScript', level: 80 },
-        { name: 'HTML/CSS', level: 75 },
-        { name: 'WordPress', level: 85 },
-      ]
+        { name: "WordPress", level: 85 },
+        { name: "WooCommerce", level: 80 },
+        { name: "JavaScript", level: 80 },
+        { name: "ReactJS", level: 75 },
+        { name: "HTML/CSS", level: 75 },
+      ],
     },
     {
       name: "Backend Development",
       roles: [
-        { name: 'Node.js', level: 70 },
-        { name: 'PHP', level: 80 },
-        { name: 'WordPress', level: 80 },
-        { name: 'SQL', level: 80 },
-        { name: 'MongoDB', level: 60 },
-      ]
+        { name: "NodeJS", level: 70 },
+        { name: "PHP", level: 80 },
+        { name: "WordPress", level: 80 },
+        { name: "SQL", level: 80 },
+        { name: "MongoDB", level: 60 },
+      ],
     },
     {
       name: "Tools & Technologies",
       roles: [
-        { name: 'Git', level: 80 },
-        { name: 'Docker', level: 85 },
-        { name: 'Figma', level: 75 },
-        { name: 'Jest', level: 85 },
-      ]
-    }
-  ]
-}
+        { name: "Git", level: 80 },
+        { name: "Gitlab", level: 75 },
+        { name: "Docker", level: 85 },
+      ],
+    },
+  ],
+};
